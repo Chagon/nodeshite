ae59cee94959837f89548caa32d603e548c28df8
Tristan Maat <tm@tlater.net>
2015-04-18 17:26:37 +0100

    Just automatically

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function toDegreeArray(roll, pitch, yaw) {
 };
 
 function radToDeg(rad) {
-    return rad * 180  / math.pi;
+    return rad * 180  / math.PI;
 }
 
 controller.on('streamingStarted', function() {
