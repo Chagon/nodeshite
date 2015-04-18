5acb2ec16eb7fcb61bb164ea569c5465da01f8d9
Tristan Maat <tm@tlater.net>
2015-04-18 17:21:51 +0100

    Just automatically

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ require('leapjs/template/entry.js');
 var controller = new Leap.Controller();
 
 controller.on('frame', function(frame) {
-    if (frame.hands === undefined)
-        console.log(frame.hands[0].roll());
+    if (frame.hands !== undefined) {
+        var roll = frame.hands[0].roll();
+        var pitch = frame.hands[0].pitch();
+        var yaw = frame.hands[0].yaw();
+
+        console.log(toDegreeArray(roll, pitch, yaw));
+    }
 });
+
+function toDegreeArray(roll, pitch, yaw) {
+    return [ radToDeg(roll), radToDeg(pitch), radToDeg(yaw) ];
+};
+
+function radToDeg(rad) {
+    return rad * 180  / math.pi;
+}
 
 controller.on('streamingStarted', function() {
     console.log('cats');
