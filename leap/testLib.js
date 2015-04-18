c726cd1eac7caf71bbaadf01f7397ec239a9850f
Tristan Maat <tm@tlater.net>
2015-04-18 21:17:21 +0100

    Restructured, OO-ialized and generally wrote files more seriously

---
+++ leap/testLib.js
@@ -0,0 +1,54 @@
+// If the numbers should be formatted neatly
+DO_FORMATING = false;
+
+var controller = new Leap.Controller();
+
+controller.on('frame', function(frame) {
+    if (frame.hands[0] !== undefined) {
+        var roll = frame.hands[0].roll();
+        var pitch = frame.hands[0].pitch();
+        var yaw = frame.hands[0].yaw();
+
+        if (DO_FORMATING) {
+            array = toDegreeArray(roll, pitch, yaw).map(function(element) {
+                return formatThreeDigitSigned(element);
+            });
+
+            firstHand = array;
+        }
+        else
+            firstHand = toDegreeArray(roll, pitch, yaw);
+        console.log(frame.hands[0].palmPosition[1]);
+    }
+});
+
+function toDegreeArray(roll, pitch, yaw) {
+    return [ radToDeg(roll), radToDeg(pitch), radToDeg(yaw) ];
+}
+
+function radToDeg(rad) {
+    return Math.floor(rad * 180  / Math.PI);
+}
+
+function formatThreeDigitSigned(number) {
+    var returnString = '' + number;
+    var length = number.toString().length;
+
+    if (number >= 0 )
+        returnString = ' ' + returnString;
+    else
+        length--;
+
+    while (length < 3) {
+        returnString = ' ' + returnString;
+        length++;
+    }
+
+    return returnString;
+}
+
+controller.on('streamingStarted', function() {
+    console.log('cats');
+});
+
+controller.connect();
